5dc3d6ad79d933af2d00bd7723d058ad1782edc1
maanrui <myl142857@yahoo.com>
2015-04-24 06:14:14 +0800

    chapter 5.4.4 but partial do not work, and change to forEach & include works well

--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,11 @@ module.exports = router;
 router.get('/hello', function(req, res) {
   res.send('The time is ' + new Date().toString());
 });
+
+router.get('/list', function(req, res){
+  res.render('list', {
+    title: 'List',
+    items: [1991, 'byvoid', 'express', 'Node.js']
+  });
+});
+
