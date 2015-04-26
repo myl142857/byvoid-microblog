df17214e00a65801dee2dc86393b6d6556a0b4fa
maanrui <myl142857@yahoo.com>
2015-04-26 15:06:48 +0800

    echo req.headers

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,11 @@ router.get('/hello', function(req, res) {
   res.send('The time is ' + new Date().toString());
 });
 
+util = require('util');
 router.get('/list', function(req, res){
   res.render('list', {
     title: 'List',
+    header: util.inspect(req.headers),
     items: [1991, 'byvoid', 'express', 'Node.js']
   });
 });
